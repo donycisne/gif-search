dbf2812ad7b4e4ad778b2f24a437309b1117adc5
Dony Cisneros <dony.cisne@gmail.com>
2019-02-28 04:48:17 -0300

    Fetching Data with the Fetch API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 
 export default class App extends Component {
+
+  state = {
+    gifs: []
+  };
+
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=smU8vGUB44RDbDcuRWlEsVftEvun349U')
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({ gifs: responseData.data });
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
   render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <Header />
