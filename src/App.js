9fbe9be712c605990a644b5de7f32c58ae1ba842
Dony Cisneros <dony.cisne@gmail.com>
2019-02-28 05:11:05 -0300

    Fetching Data with Axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './components/Header';
 import MainContent from './components/MainContent';
@@ -10,18 +11,16 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=smU8vGUB44RDbDcuRWlEsVftEvun349U')
-    .then(response => response.json())
-    .then(responseData => {
-      this.setState({ gifs: responseData.data });
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=smU8vGUB44RDbDcuRWlEsVftEvun349U')
+      .then(response => {
+        this.setState({ gifs: response.data.data })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing the data', error);
+      });
   }
 
   render() {
-    console.log(this.state.gifs);
     return (
       <div>
         <Header />
